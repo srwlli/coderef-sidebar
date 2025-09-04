def2306a02340f699150b46eef6a663db18673ca
srwlli <will.hart.sr@icloud.com>
2025-09-04 19:20:17 -0400

    organized components structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
+} from '@/components/cards';
 import Link from 'next/link';
 
 export default function Home() {
